ed641a04efc94b7c1fa44b96c4b932db84212e81
Alex100688 <alessiozingaro2@gmail.com>
2022-11-02 20:47:32 +0100

    Aggiunge un evento per poter giocare al gioco

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 "use strict";
-const campoBox=document.querySelector(".campo-box")
-for (let i = 1; i <=100; i++) {
-    const boxCell=document.createElement("div");
-    boxCell.innerHTML=(i);
-    boxCell.classList.add("cell-box");
-    campoBox.append(boxCell);
-}
+const displayBox=document.querySelector(".display-box");
+const giocoPlay=document.querySelector(".btn-play");
+giocoPlay.addEventListener(`click`, function(){
+    displayBox.classList.toggle("display-box");
+    const campoBox=document.querySelector(".campo-box");
+    for (let i = 1; i <=100; i++) {
+        const cellBox=document.createElement("div");
+        cellBox.innerHTML=(i);
+        cellBox.classList.add("cell-box");
+        campoBox.append(cellBox);
+    }
+})
